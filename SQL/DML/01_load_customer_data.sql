9bc39bd7fb541d9b21758bd9c68973d1631840bf
arinmac26-sys <arinmac26@gmail.com>
2026-08-10 10:50:30 +0530

    Add files via upload

--- SQL/DML/01_load_customer_data.sql
+++ SQL/DML/01_load_customer_data.sql
@@ -1,1 +1,14 @@
+-- Portfolio load template.
+-- Bulk-load commands vary by MySQL client/environment.
+-- Example:
 
+LOAD DATA LOCAL INFILE 'Customer_Master_Data.csv'
+INTO TABLE customer_master
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'Customer_Transactions.csv'
+INTO TABLE customer_transactions
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS
+(CustomerID, TransactionDate, TransactionAmount);
